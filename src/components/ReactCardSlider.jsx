7db5a624b922c62715b4a4326a6f825292bf71d3
Oliver Kientzler <kientzler.oliver@gmail.com>
2023-04-12 20:13:32 +0200

    update card and try url image don't work, slider finish

--- src/components/ReactCardSlider.jsx
+++ src/components/ReactCardSlider.jsx
@@ -2,58 +2,76 @@ import React from "react";
 import "./Slider.css";
 
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
-
+// npm install react-icons --save
 const ReactCardSlider =(props)=>{
   const articles = [
     {
       title: "Cup",
       price: "19$",
-      image: "card1",
+      image: "./images/card1.jpg",
     },
     {
       title: "Sweat",
       price: "39$",
-      image: "card2",
+      image: "./images/card2.jpg",
     },
     {
       title: "Bag",
       price: "15$",
-      image: "card3",
+      image: "./images/card3.jpg",
     },
     {
       title: "T-shi",
       price: "23$",
-      image: "card4",
+      image: "./images/card4.jpg",
     },
     {
       title: "Poster",
       price: "10$",
-      image: "card5",
+      image: "./images/card5.jpg",
     },
     {
       title: "T-shirt",
       price: "22$",
-      image: "card6",
+      image: './images/card6.jpg',
+
     },
   ];
+
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  }
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  }
   return (
     <div className="main-slider-container">
-      <MdChevronLeft size={40} className="slider-icon left"/>
-      <div className="slider">
+      <MdChevronLeft size={40} className="slider-icon left" onClick={slideLeft}/>
+      <div id="slider">
         { 
         articles.map((article,index)=>{
           return(
-            <div className="slider-card">
+            <div className="slider-card" key={index}>
+              
+              <div className="slider-card-image" style={{backgroundImage: `url(${article.image})`}} >
+              </div>
+              <div className="slider-card-title">
+                <p>{article.title}</p>
+              </div>
+              <div className="slider-card-price">
+                <p>{article.price}</p>
+              </div>
               
             </div>
           )
         })
         }
       </div>
-      <MdChevronRight size={40} className="slider-icon right"/>
+      <MdChevronRight size={40} className="slider-icon right" onClick={slideRight}/>
     </div>
   )
 }
 export default ReactCardSlider;
 
-// npm install react-icons --save
